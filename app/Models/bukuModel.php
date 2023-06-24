f4a12021d6cf8d392c670fcc702b3be06fc6401d
Farraskuy <farrasfadhil06@gmail.com>
2023-06-24 14:32:52 +0700

    menambahkan migration dan model buku

---
+++ app/Models/bukuModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BukuModel extends Model
+{
+    protected $table      = 'buku';
+    protected $useTimestamps = true;
+    protected $allowedFields = ['judul', 'slug', 'penulis', 'penerbit', 'tanggal_terbit', 'sampul', 'sinopsis'];
+
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    // protected $useSoftDeletes = true;
+
+    // Dates
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function ambilBuku($slug) {
+        return $this->where('slug', $slug)->first();
+    }
+}
