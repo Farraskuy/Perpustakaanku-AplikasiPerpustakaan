7fa776b7619a55d0b2cde778c2b5ead7db43f642
Farraskuy <farrasfadhil06@gmail.com>
2023-06-26 14:39:10 +0700

    menambahkan migration, seeder, dan model petugas

---
+++ app/Database/Migrations/2023-06-26-073500_Petugas.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Petugas extends Migration
+{
+    public function up()
+    {
+
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'foto' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'jenis_kelamin' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'agama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'jabatan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'alamat' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('petugas');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('petugas');
+    }
+}
